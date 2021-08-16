42a190b46538fdf104242e4c71c7d2420d3b5e16
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2021-08-16 23:37:55 +0900

    0816 메모장 프로그램

---
+++ gui_basic/메모장프로그램.py
@@ -0,0 +1,52 @@
+# 메뉴
+import os
+from tkinter import *
+import tkinter.messagebox as msgbox
+
+root = Tk()
+root.title("제목 없음 - Windows 메모장") # 제목 설정
+root.geometry("640x400")
+
+menu = Menu(root)
+
+filename = "mynote.txt"
+
+def loadFile():
+    if os.path.isfile(filename):
+        with open(filename, "r", encoding="utf8") as file:
+            txt.delete("1.0", END) # 텍스트 위젯 본문 삭제
+            txt.insert(END, file.read()) # 파일 내용을 본문에 입력
+def saveFile():
+    with open(filename, "w", encoding="utf8") as file:
+        file.write(txt.get("1.0", END)) # 모든 내용을 가져와서 저장
+    msgbox.showinfo("알림", "정상적으로 저장되었습니다.")
+
+
+#File 메뉴
+menu_file = Menu(menu, tearoff=0)
+menu_file.add_command(label="열기", command=loadFile)
+menu_file.add_command(label="저장", command=saveFile)
+menu_file.add_separator()
+
+menu_file.add_command(label="끝내기",command=root.quit)
+menu.add_cascade(label="파일",menu=menu_file)
+
+# Edit 메뉴 빈값
+menu.add_cascade(label="편집")
+menu.add_cascade(label="서식")
+menu.add_cascade(label="보기")
+menu.add_cascade(label="도움말")
+
+# 스크롤 바
+scrollbar = Scrollbar(root)
+scrollbar.pack(side="right", fill="y")
+
+# 본문 영역
+txt = Text(root, yscrollcommand=scrollbar.set)
+txt.pack(side="left",fill="both", expand=True)
+
+scrollbar.config(command=txt.yview)
+
+
+root.config(menu=menu)
+root.mainloop()
