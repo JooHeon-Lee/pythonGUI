4afbbcd966d0a5539742ae4a00a7449e458016b4
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2021-08-29 22:39:45 +0900

    0829 기본함수 설정 및 이미지 자동캡쳐 유틸프로그램 추가

--- gui_project/2_basic_function.py
+++ gui_project/2_basic_function.py
@@ -1,6 +1,7 @@
 # 기본 프레임
 import builtins
 import tkinter.ttk as ttk
+import tkinter.messagebox as msgbox
 from tkinter import *
 from tkinter import filedialog
 
@@ -23,6 +24,33 @@ def del_file():
 
     for index in reversed(list_file.curselection()):
         list_file.delete(index)
+
+# 저장 경로 (폴더)
+def browse_dest_file():
+    folder_selected = filedialog.askdirectory()
+    if folder_selected is None: # 사용자가 취소를 누를 때
+        return
+    # print(folder_selected)
+    txt_dest_path.delete(0, END)
+    txt_dest_path.insert(0, folder_selected)
+
+# 시작
+def start():
+    # 각 옵션들 값을 확인
+    print("가로넓이 : ", cmb_width.get())
+    print("간격 : ", cmb_space.get())
+    print("포맷 : ", cmb_format.get())
+
+    # 파일 목록 확인
+    if list_file.size() == 0:
+        msgbox.showwarning("경고", "이미지 파일을 추가하세요")
+        return
+
+    # 저장 경로 비어있는지 확인
+    if len(txt_dest_path.get()) == 0:
+        msgbox.showwarning("경고", "저장 경로를 선택하세요.")
+        return
+    
 
 # 파일 프레임 (파일 축, 선택 삭제)
 file_frame = Frame(root)
@@ -52,7 +80,7 @@ path_frame.pack(fill="x", padx=5, pady=5, ipady=5)
 txt_dest_path = Entry(path_frame)
 txt_dest_path.pack(side="left", fill="x", expand=True, ipady=4, padx=5, pady=5) # 높이변경
 
-btn_dest_path = Button(path_frame, text="찾아보기", width=10)
+btn_dest_path = Button(path_frame, text="찾아보기", width=10, command=browse_dest_file)
 btn_dest_path.pack(side="right", padx=5, pady=5)
 
 # 옵션 프레임
@@ -107,7 +135,7 @@ frame_run.pack(fill="x", padx=5, pady=5)
 btn_close = Button(frame_run, padx=5, pady=5, text="닫기", width=12, command=root.quit)
 btn_close.pack(side="right", padx=5, pady=5)
 
-btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12)
+btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12, command=start)
 btn_start.pack(side="right", padx=5, pady=5)
 
 
